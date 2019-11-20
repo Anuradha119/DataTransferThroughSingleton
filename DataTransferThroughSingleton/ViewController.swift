bddb7c540f95fe045cffffcd395a1bb217bb63cc
Marni Anuradha <marnianuradha@achiever-111.local>
2019-11-20 16:24:03 +0530

    Initial Commit

---
+++ DataTransferThroughSingleton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataTransferThroughSingleton
+//
+//  Created by Marni Anuradha on 11/20/19.
+//  Copyright © 2019 Marni Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
